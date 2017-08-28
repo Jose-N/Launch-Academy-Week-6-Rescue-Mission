53d54bed0819666f300821f315420dd7253b649f
Jose Naylor <jose.c.naylor@gmail.com>
2017-08-28 14:04:54 -0400

    added index and show pages for qestions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,9 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.order('created_at DESC')
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+end
---
+++ spec/factories/questions.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :question do
+    name "MyString"
+    description "MyString"
+  end
+end
